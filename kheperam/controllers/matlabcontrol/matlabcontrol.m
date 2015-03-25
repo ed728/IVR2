d720a542d26830b778df963c52dc424c8f85c91c
ed728 <edvin.jaras@gmail.com>
2015-03-25 15:02:47 +0000

    My stuff

---
+++ kheperam/controllers/matlabcontrol/matlabcontrol.m
@@ -0,0 +1,79 @@
+% Very simple MATLAB controller for Webots
+% File: matlabcontrol.m
+% Date: 21/09/2011          
+% Description: This controller will open MATLAB and let you input
+%              commands from the keyboard
+% Author: Simon Smith (artificialsimon@ed.ac.uk)        
+% Modifications: 
+% Use: command to set motor speed 1 to left motor, -1 to right motor
+%       wb_differential_wheels_set_speed(1, -1)
+% After sending a setting command, the controller have to resume by
+% sending "return" in the keyboard input
+% This will allow a sensor value update as well
+
+TIME_STEP = 64;
+N = 8;
+SPEED = 4;
+TURN_THRESH = 15;
+BLOCK_THRESH = 75;
+% get and enable distance sensors
+for i=1:N
+  ps(i) = wb_robot_get_device(['ds' int2str(i-1)]);
+  wb_distance_sensor_enable(ps(i),TIME_STEP);
+end
+
+% Calling MATLAB desktop versionwb_differential_wheels_set_speed(1, -1);
+% desktop;
+
+% main loop:
+% perform simulation steps of TIME_STEP milliseconds
+% and leave the controll to the keyboard
+while wb_robot_step(TIME_STEP) ~= -1
+  %Original example 
+  %{
+  % read all distance sensors
+       for i=1:N
+           sensor_values(i) = wb_distance_sensor_get_value(ps(i));
+       end
+  % display all distance sensors values
+       sensor_values
+  left_speed=1;
+  right_speed=1;
+  %set_speeds(left_speed,right_speed);
+  if sensor_values(1)+sensor_values(2)+sensor_values(3)>10
+    wb_differential_wheels_set_speed(SPEED, -SPEED);
+  elseif sensor_values(4)+sensor_values(5)+sensor_values(6)>10
+    wb_differential_wheels_set_speed(-SPEED, SPEED);
+  else
+     wb_differential_wheels_set_speed(SPEED, SPEED);
+  end;
+  %control goes to the keyboard
+  %keyboard;
+  %}
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %Read appropriate sensors
+  for i=1:N
+      sensor_values(i) = wb_distance_sensor_get_value(ps(i));
+  end
+  %left side clockwise: 1(side), 2, 3(front)
+  %right side clockwise: 4(front), 5, 6(side)
+  %back clockwise: 7(right), 8(left)
+  left = sensor_values(1) + sensor_values(2);
+  right = sensor_values(5) + sensor_values(6);
+  front = sensor_values(3) + sensor_values(4);
+  
+  %Control logic
+  if(left + right + front < TURN_THRESH)
+      %EMPTY road, just go FW
+  elseif(left > BLOCK_THRESH && right > BLOCK_THRESH && front > BLOCK_THRESH)
+       %Turn around (right)
+  elseif(right - left > BLOCK_THRESH && front > BLOCK_THRESH)
+      %Turn left
+  elseif(left-right > BLOCK_THRESH && front > BLOCK_THRESH)
+      %Turn right
+  elseif(front > BLOCK_THRESH)
+      %Turn tight (this is default case, where going fw is blocked, but we
+      %can turn either direction.)
+  elseif()
+  end
+end
